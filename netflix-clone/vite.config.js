1d45fc58b9fecf5d8899cdebdce7c91182786113
yoniwolde <yoniwol89@gmail.com>
2024-09-16 14:19:34 -0400

    update vite

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:"/",
+  base: "/Netflix-Clone2024/",
   plugins: [react()],
-})
+});
